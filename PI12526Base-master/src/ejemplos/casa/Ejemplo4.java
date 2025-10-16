abf4a895706bbe0795cab7722b337d0709d5a32f
sonde-mllr <c8534312@gmail.com>
2025-10-16 19:50:15 +0200

    hice parte del ej4 en casa

---
+++ PI12526Base-master/src/ejemplos/casa/Ejemplo4.java
@@ -0,0 +1,49 @@
+package ejemplos.casa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ejemplo4 {
+	public static Double potAN_recNoFinal(Double a, Integer n) {
+		Double res = null;
+		if(n==0) {
+			return 1.;
+		} else {
+			res = a*potAN_recNoFinal(a,n-1);
+		}
+		return res;
+	}
+	
+	public static Double potAN_it(Double a,Integer n) {
+		Double res = a;
+		while(n > 1) {
+			res *=a;
+			n-=1;
+		}
+		return res;
+	}
+	
+	 // porque me apetece
+	
+	public static Double potAN_recFinal(Double a,Integer n) {
+		Double res = null;
+		if(n==1) {
+			return a;
+		} else {
+			res = potAN_recFinal(a+a,n-1);
+		}
+		return res;
+	}
+	
+
+	
+	public static Double potAN_it2(Double a,Integer n) {
+		Double res = a;
+		for(int i = 0; i<n-1;i++) {
+			res *= a;
+		}
+		return res;
+	}
+	
+	
+}
---
+++ PI12526Base-master/src/ejemplos/casa/tests/ejemplo4Test.java
@@ -0,0 +1,15 @@
+package ejemplos.casa.tests;
+
+import ejemplos.casa.Ejemplo4;
+
+public class ejemplo4Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Ejemplo4.potAN_recNoFinal(2.,20));
+		System.out.println(Ejemplo4.potAN_it(2.,20));
+		System.out.println(Ejemplo4.potAN_recFinal(2.,20));
+		System.out.println(Ejemplo4.potAN_it2(2.,20));
+
+	}
+
+}
